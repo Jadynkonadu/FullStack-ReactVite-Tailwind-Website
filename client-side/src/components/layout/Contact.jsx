84ef6557d25e7be087c43dff18e3671154cf2f0c
Jadyn Asamoah <jadynkonadu@gmail.com>
2024-03-05 08:16:47 -0500

    Edited About and Contact

--- client-side/src/components/layout/Contact.jsx
+++ client-side/src/components/layout/Contact.jsx
@@ -1,4 +1,3 @@
-import devan from './images/devan.png'
 import jadyn from './images/jadyn.png'
 import abigail from './images/abigail.png'
 
@@ -30,36 +29,13 @@ const Contact = () => {
                     </div>
                 </div>
 
-            <div className="bg-gradient-to-r  from-gray-200/90 to-gray-200/50  rounded-lg shadow-xl flex w-1/3 ml-4 transition-transform hover:scale-105 mt-10">
-                    <div className="mx-auto w-1/3 self-center">
-                        <img src={devan} alt="Profile" />
-                    </div>
-                    <div className="mt-1 mx-auto w-2/3 text-center">
-                        <p className="text-sm text-gray-800 leading-5 md:text-base pb-1 pt-3">Devan Spears</p>
-                        <p className="text-xs text-neutral-500 leading-5 md:text-sm pb-1 pt-1">WCCI Student</p>
-                        <p className="text-xs text-gray-500 leading-5 md:text-sm pt-1">devan.spears520@gmail.com</p>
-                        <p>
-                            <a href="https://github.com/DevSpe64" target='_blank' rel='noopener noreferrer' className="inline-flex items-center text-slate-400 hover:underline p-1 text-xs md:text-sm">
-                                GitHub
-                                <path stroke="currentColor" />
-                            </a>
-                        </p>
-                        <p>
-                            <a href="https://www.linkedin.com/in/devan-spears-24712a195/" target='_blank' rel='noopener noreferrer' className="pb-1 text-xs md:text-sm inline-flex items-center text-slate-400 hover:underline">
-                                LinkedIn
-                                <path stroke="currentColor" />
-                            </a>
-                        </p>
-                    </div>
-                </div>
-
             <div className="bg-gradient-to-r from-gray-200/90 to-gray-200/50  rounded-lg shadow-xl flex w-1/3 ml-4 transition-transform hover:scale-105 mt-10">
                     <div className="mx-auto w-1/3 self-center">
                         <img src={jadyn} alt="Profile" />
                     </div>
                     <div className="mt-1 mx-auto w-2/3 text-center">
                         <p className="text-sm text-gray-800 leading-5 md:text-base pb-1 pt-3">Jadyn Asamoah</p>
-                        <p className="text-xs text-neutral-500 leading-5 md:text-sm pb-1 pt-1">WCCI Student</p>
+                        <p className="text-xs text-neutral-500 leading-5 md:text-sm pb-1 pt-1">IT Field Technician</p>
                         <p className="text-xs text-gray-500 leading-5 md:text-sm pt-1">jadynkonadu@gmail.com</p>
                         <p>
                             <a href="https://github.com/Jadynkonadu" target='_blank' rel='noopener noreferrer' className="inline-flex items-center text-slate-400 hover:underline p-1 text-xs md:text-sm">
